81f38a06da00556531e812cb5423e06e17717094
Pratishtha Singh <pratishthasingh@MacBook-Pro.attlocal.net>
2020-10-27 21:37:31 -0500

    multiple points data

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
             // An alternative to using the setView() method is to modify each attribute in the map object using the curly braces notation as follows:
 
@@ -15,14 +15,61 @@ let map = L.map('mapid').setView([40.7, -94.5], 14);
             // });
             // this is useful to add multiple tile layers
 
-L.circleMarker([34.0522, -118.2437], {
-   radius: 300,
-   color: "black",
-   fillColor: '#ffffa1'
-}).addTo(map);
+ // iterate by : for (let i=0; i < cities.length; i++)
+
+ L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+ }).addTo(map);
+ 
+ let cityData = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
+
+ // Loop through the cities array and create one marker for each city.
+ cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+      radius: city.population/100000,
+      color: "orange"
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+ // or by cities.forEach(function(city)) to add each location to the marker() function
+
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken})', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
